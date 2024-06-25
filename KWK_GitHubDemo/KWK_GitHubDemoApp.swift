00dfa3cf8380b78fb46d10f1d1095fc57ddec27e
francescaceccarelli <francescamceccarelli@gmail.com>
2024-06-25 10:03:38 -0700

    Initial Commit

---
+++ KWK_GitHubDemo/KWK_GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_GitHubDemoApp.swift
+//  KWK_GitHubDemo
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
